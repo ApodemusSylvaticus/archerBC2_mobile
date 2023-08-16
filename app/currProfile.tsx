a1f19571f913e0d57bc7673586559d14285acaeb
ApodemusAgrarius <reanimator737@gmail.com>
2023-08-16 09:22:52 +0200

    prebuild

--- app/currProfile.tsx
+++ app/currProfile.tsx
@@ -2,11 +2,8 @@ import React from 'react';
 import { ScrollView } from 'react-native';
 import { DefaultContainer } from '@/components/container/defaultBox';
 import { Profile } from '@/components/profile';
-import { useProfileStore } from '@/store/useProfileStore';
 
 const CurrProfile: React.FC = () => {
-    const profileStore = useProfileStore();
-    console.log('profileStore', profileStore);
     return (
         <ScrollView>
             <DefaultContainer>
