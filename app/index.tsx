8051a4f237cee0e3325dbc5ba26535b85a9bec36
ApodemusAgrarius <reanimator737@gmail.com>
2023-08-16 09:26:39 +0200

    fix

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
 import { Redirect } from 'expo-router';
 import React from 'react';
+import { Routing } from '@/constant/routing';
 
 export default function Home() {
-    return <Redirect href="/profiles" />;
+    return <Redirect href={Routing.PROFILES} />;
 }
